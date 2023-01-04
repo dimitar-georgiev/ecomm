3638329ba65af1922621ad9b2b7e28548632082f
Dimitar Georgiev <dimiter_georgiev@mail.bg>
2023-01-03 18:41:43 -0800

    1:15:15_useReducer

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import morgan from 'morgan';
 
 import {data} from './data.js';
 
 const app = express();
+app.use(morgan('dev'));
 
 app.get('/api/products', (req, res) => {
     res.send(data.products)
