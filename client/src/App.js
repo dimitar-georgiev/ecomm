e72a19f2bc21b4ed8e1c3846bf37bcd98bafe1e5
Dimitar Georgiev <dimiter_georgiev@mail.bg>
2023-01-03 20:31:07 -0800

    1:34:14_bootstrap_card_for_product

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {Container, Navbar} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
@@ -6,17 +8,30 @@ import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className='d-flex flex-column site-container'>
         <header>
-          <Link to="/">amazona</Link>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>
+                  amazona
+                </Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+          {/* <Link to="/">amazona</Link> */}
         </header>
         <main>
-          <Routes>
-            <Route path='/product/:slug' element={<ProductScreen />} />
-            <Route path='/' element={<HomeScreen />} />
-          </Routes>
-          
+          <Container>
+            <Routes>
+              <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path='/' element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>All rights reserved.</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
