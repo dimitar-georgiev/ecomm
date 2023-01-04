02d6de750cf805f140ca9a682dda2bf158be7993
Dimitar Georgiev <dimiter_georgiev@mail.bg>
2023-01-03 17:38:02 -0800

    50:14_rendering_products

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {data} from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <a href="/">amazona</a>
       </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className='products'>
+          {data.products.map(product => (
+            <div key={product.slug} className='product'>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className='product-info'>
+                <a href={`/product/${product.slug}`}>
+                  <p>
+                    {product.name}
+                  </p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
